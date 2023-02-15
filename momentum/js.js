b7a3b4b95385c3a002aa40efae9ba4317e61e6c2
Inna Didenko <innadidenko29@gmail.com>
2023-02-15 22:10:04 +0200

    update: js file

--- momentum/js.js
+++ momentum/js.js
@@ -1,14 +1,100 @@
 const time = document.querySelector('.time')
- const date = new Date()
+const dateStr = document.querySelector('.date')
+const greeting = document.querySelector ('.greeting')
+const name = document.querySelector ('.name')
+const body = document.querySelector('body')
 
-// const currentTime = date.toLocaleTimeString()
-// console.log(currentTime)
 
-setInterval (function () {
-    document.querySelector('.time').innerHTML = date_time();
-    
-}, 100)
 
+
+
+
+
+function showTime() {
+    const date = new Date()
+    const currentTime = date.toLocaleTimeString()
+    time.textContent = currentTime
+    setTimeout(showTime, showDate, getTimeOfDay, 1000)
+ }
+showTime()
+ 
+
+function showDate() {
+    const date = new Date()
+    const option = { weekday: 'long', month: 'long', day: 'numeric'}
+    const currentDate = date.toLocaleDateString('en-US', option)
+    dateStr.textContent = currentDate
+} 
+showDate()
+
+
+function getTimeOfDay() {
+    const date = new Date()
+    const hours = date.getHours()
+    if (hours < 4) {
+     greeting.textContent = `Good nigth`
+    }
+    else if (hours < 12) {
+      greeting.textContent = `Good morning`
+    }
+    else if (hours < 17) {
+     greeting.textContent = `Good afternoon`
+    }
+    else if (hours < 24) {
+     greeting.textContent = `Good evening`
+    }
+}
+getTimeOfDay()
+
+
+function setLocalStorage() {
+    localStorage.setItem('name', name.value);     
+}
+window.addEventListener('beforeunload', setLocalStorage)
+
+function getLocalStorage() {
+    if (localStorage.getItem('name')) {
+        name.value = localStorage.getItem('name')
+    }
+}
+window.addEventListener('load', getLocalStorage)  
+
+
+function getRandomNum(min, max){
+    (Math.floor(( Math.random()*(max-min)+min)))
+}
+getRandomNum(1, 21) 
+
+
+
+function setBg() {
+      const date = new Date()
+    let timeOfDay = date.getHours()
+    let bgNum = Math.random()
+   console.log (bgNum)
+  
+    if (timeOfDay < 4) {
+     body.style.backgroundImage = "url('https://raw.github.com/inna38/stage1-tasks/assets/images/night/01.jpg')"
+    }
+    else if (timeOfDay < 12) {
+       body.style.backgroundImage = "url('https://raw.github.com/inna38/stage1-tasks/assets/images/morning/01.jpg')"
+    }
+    else if (timeOfDay < 17) {
+    body.style.backgroundImage = "url('https://raw.github.com/inna38/stage1-tasks/assets/images/afternoon/01.jpg')"
+    }
+    else if (timeOfDay < 24) {
+    return body.style.backgroundImage = Math.floor(( Math.random()*("url('https://raw.github.com/inna38/stage1-tasks/assets/images/evening/.jpg'))")))
+     
+    }
+  }
+setBg()
+
+
+// setInterval(() => {
+//     const date = new Date()
+//     const currentTime = date.toLocaleTimeString()
+//     time.textContent = currentTime
+// }, 1000);
 
 // const pause = document.querySelector('.pause')
 // const play = document.querySelector('.play')
