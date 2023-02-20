6c95b6e249076a0a11a0dbac910e600bae85cd64
Inna Didenko <innadidenko29@gmail.com>
2023-02-20 19:43:56 +0200

    update: feat js

--- momentum/js.js
+++ momentum/js.js
@@ -11,6 +11,11 @@ const weatherDescription = document.querySelector('.weather-description')
 const humidity = document.querySelector('.humidity')
 const wind = document.querySelector('.wind')
 let city = document.querySelector('.city')
+let langEn = document.querySelector('.lang-en')
+ let langRu = document.querySelector('.lang-ru')
+// let lang = document.querySelector('.lang')
+
+
 
 // async function getWeather() {
 //     const url = `https://api.openweathermap.org/data/2.5/weather?q=Минск&lang=en&appid=65fd0659067eb109288af36316a63508&units=metric`;
@@ -72,9 +77,13 @@ function showTime() {
     const date = new Date()
     const currentTime = date.toLocaleTimeString()
     time.textContent = currentTime
-    getTimeOfDay()
-    showDate()
-     getWeatherResult()
+     getTimeOfDay()
+     showDate()
+    getWeatherResult()
+    // setBg()
+    // quoteFn()
+    // getTimeOfDayRu()
+    // getTimeOfDayEn()
     setTimeout(showTime, 1000)
     
  }
@@ -90,9 +99,113 @@ function showDate() {
 showDate()
 
 
+//------------------------------------------------------------------------------------------
+//     let greetingTranslation = {
+
+//    'ru': {
+//             ru: 'Добрый день',
+//             ru: 'Добрій вечер'
+          
+//      }
+        
+// };
+// let hash = window.location.hash
+// hash = hash
+// console.log(hash)
+
+// langRu.addEventListener('click', () => {
+//     greeting.innerHTML = greetingTranslation.ru;
+// })
+
+
+//     langRu.addEventListener('click', () => {
+       
+//         const date = new Date()
+//         const hours = date.getHours()
+//             const option = { weekday: 'long', month: 'long', day: 'numeric'}
+//     const current = date.toLocaleDateString('ru-Ru', option)
+//     dateStr.textContent = current
+//     if (hours < 6) {
+//      greeting.textContent = `Доброй ночи, `
+//     }
+//     else if (hours < 12) {
+//       greeting.textContent = `Доброе утро, `
+//     }
+//     else if (hours < 18) {
+//         greeting.textContent = `Добрый день, `
+           
+//     }
+//     else if (hours < 24) {
+//      greeting.textContent = `Добрый вечер, `
+//         }
+//     })
+    
+//     langEn.addEventListener('click',() => {
+//         const date = new Date()
+//     const hours = date.getHours()
+//     if (hours < 6) {
+//      greeting.textContent = `Good nigth, `
+//     }
+//     else if (hours < 12) {
+//       greeting.textContent = `Good morning, `
+//     }
+//     else if (hours < 18) {
+//         greeting.textContent = `Good afternoon, `
+           
+//    }
+//     else if (hours < 24) {
+//      greeting.textContent = `Good evening, `
+//     }})
+
+
+
+// function getTimeOfDayRu(){
+//    const date = new Date()
+//     const hours = date.getHours()
+//     if (hours < 6) {
+//      greeting.textContent = `Good nigth, `
+//     }
+//     else if (hours < 12) {
+//       greeting.textContent = `Good morning, `
+//     }
+//     else if (hours < 18) {
+//      greeting.textContent = `Добрый день `
+//     }
+//     else if (hours < 24) {
+//      greeting.textContent = `Good evening, `
+//     }
+// }
+
+
+
+// function getTimeOfDayRu() {
+//     if (greeting.textContent = `Good eveneeng, `) {
+//         greeting.textContent.remove = `Good eveneeng, `
+//         greeting.textContent = 'Добрый день'
+//     }
+// }
+
+// function getTimeOfDayEn() {
+//      if (greeting.textContent = 'Добрый день') {
+//         greeting.textContent = `Good eveneeng, `
+//     }
+// }
+
+
+// langRu.addEventListener('click', getTimeOfDay)
+// langEn.addEventListener('click', getTimeOfDayEn)
+// langRu.addEventListener('click', getTimeOfDayRu)
+
+//----------------------------------------------------------------------------------------------
+
+
+
+
+//  --------------------------------------------------------------------------------------------------
 
 
 function getTimeOfDay() {
+
     const date = new Date()
     const hours = date.getHours()
     if (hours < 6) {
@@ -102,13 +215,18 @@ function getTimeOfDay() {
       greeting.textContent = `Good morning, `
     }
     else if (hours < 18) {
-     greeting.textContent = `gfgfgfg`
+        greeting.textContent =  `Good afternoon, `
+            
     }
     else if (hours < 24) {
      greeting.textContent = `Good evening, `
-    }
+        }
+
+
+
+
 }
-getTimeOfDay(en)
+getTimeOfDay()
 
 
 function setLocalStorage() {
@@ -230,7 +348,7 @@ function setBg() {
       const date = new Date()
     let timeOfDay = date.getHours()
 
-      if (timeOfDay < 4) {
+      if (timeOfDay < 6) {
           body.style.backgroundImage = night[Math.floor(Math.random() * night.length)]
           
  function getSlaiderNex() {
@@ -259,7 +377,7 @@ function getSlaiderPrev() {
 slidePrev.addEventListener('click', getSlaiderPrev)          
            
     }
-    else if (timeOfDay < 17) {
+    else if (timeOfDay < 18) {
           body.style.backgroundImage = afternoon[Math.floor(Math.random() * afternoon.length)]
 
           function getSlaiderNex() {
@@ -488,222 +606,91 @@ function firstAudio() {
 play.addEventListener('click', playAudio)
 last.addEventListener('click', lastAudio)
 first.addEventListener('click', firstAudio)
+
  
 
+let quote = document.querySelector('.quote')
+let changeQuote = document.querySelector('.change-quote')
+let author = document.querySelector('.author')
+let dataQuotes = [  
+    '"Genius is one percent inspiration and ninety-nine percent perspiration."  Thomas Edison',
+    '"You can observe a lot just by watching."  Yogi Berra',
+    '"A house divided against itself cannot stand."  Abraham Lincoln',  
+  '"Difficulties increase the nearer we get to the goal."  Johann Wolfgang von Goethe',  
+   '"Fate is in your hands and no one elses"  Byron Pulsifer',
+    '"Be the chief but never the lord."  Lao Tzu',
+    '"Nothing happens unless first we dream."  Carl Sandburg',
+      '"Well begun is half done."  Aristotle',
+    '"Life is a learning experience, only if you learn."  Yogi Berra',
+        '"Self-complacency is fatal to progress."  Margaret Sangster',
+    '"Peace comes from within. Do not seek it without."  Buddha',
+    '"What you give is what you get." Byron Pulsifer',  
+ '"We can only learn to love by loving." Iris Murdoch',
+ ' "Life is change. Growth is optional. Choose wisely." Karen Clark', 
+     ' "You ll see it when you believe it." Wayne Dyer',  
+       ' "To lead people walk behind them." Lao Tzu',  
+  ' "Having nothing, nothing can he lose."  William Shakespeare', 
+   ' "Trouble is only opportunity in work clothes."  Henry J. Kaiser',
+    ' "A rolling stone gathers no moss."  Publilius Syrus',    
+    ' "Ideas are the beginning points of all fortunes." Napoleon Hill', 
+    '"Everything in life is luck."  Donald Trump',   
+   '"Doing nothing is better than being busy doing nothing." Lao Tzu',
+   ' "Trust yourself. You know more than you think you do."  Benjamin Spock',
+   '"Study the past, if you would divine the future."  Confucius',  
+   ' "From error to error one discovers the entire truth."  Sigmund Freud',
+  ' "Well done is better than well said."  Benjamin Franklin',
+   '"Bite off more than you can chew, then chew it."  Ella Williams',   
+   '   "Work out your own salvation. Do not depend on others."  Buddha',
+   ' "One today is worth two tomorrows."  Benjamin Franklin',
+    ' "Once you choose hope, anythings possible."  Christopher Reeve',  
+   ' "God always takes the simplest way."  Albert Einstein',  
+      '"One fails forward toward success."  Charles Kettering',  
+   '"Learning is a treasure that will follow its owner everywhere"  Chinese proverb',
+      ' "Be as you wish to seem."  Socrates',  
+ ' "The world is always in movement."  V. Naipaul',
+  ' "Never mistake activity for achievement."  John Wooden',  
+    ' "What worries you masters you."  Haddon Robinson', 
+  ' "One faces the future with ones past."  Pearl Buck',  
+    ' "Goals are the fuel in the furnace of achievement."  Brian Tracy', 
+     ' "Who sows virtue reaps honour."  Leonardo da Vinci',  
+     ' "Be kind whenever possible. It is always possible."  Dalai Lama',   
+     ' "Talk doesnt cook rice."  Chinese proverb',
+     ' "He is able who thinks he is able."  Buddha',
+      '"A goal without a plan is just a wish."  Larry Elder',
+       '"To succeed, we must first believe that we can."  Michael Korda',
+        '"Learn from yesterday, live for today, hope for tomorrow."  Albert Einstein',
+      ' "A weed is no more than a flower in disguise."  James Lowell',
+ ' "Do, or do not. There is no try."  Yoda',
+    ' "All serious daring starts from within."  Harriet Beecher Stowe',
+      '"The best teacher is experience learned from failures."  Byron Pulsifer',
+       ' "Think how hard physics would be if particles could think."  Murray Gell-Mann',
+   '"Love is the flower youve got to let grow."  John Lennon',
+       '"Dont wait. The time will never be just right."  Napoleon Hill',
+  '"Time is the wisest counsellor of all."  Pericles',
+      '"You give before you get."  Napoleon Hill',
+        ' "Wisdom begins in wonder."  Socrates',
+   ' "Without courage, wisdom bears no fruit."  Baltasar Gracian',
+       '"Change in all things is sweet."  Aristotle',
+       '"What you fear is that which requires action to overcome."  Byron Pulsifer',
+       ' "When performance exceeds ambition, the overlap is called success."  Cullen Hightower',
+      '"When deeds speak, words are nothing."  African proverb',
+   '"Real magic in relationships means an absence of judgement of others."  Wayne Dyer',
+'"I never think of the future. It comes soon enough."  Albert Einstein',
+   '"Skill to do comes of doing."  Ralph Emerson',
+    '"Wisdom is the supreme part of happiness."  Sophocles',
+  '"I believe that every person is born with talent."  Maya Angelou',
+      ' "Important principles may, and must, be inflexible."  Abraham Lincoln',
+    ]
 
-// pause.addEventListener('click', pauseAudio )
 
 
-
-    // {
-    // title: "Aqua Caelestis",
-    // srs:  "./assets/sounds/Aqua Caelestis.mp3"
-    // },
-    // {
-    // title: "Ennio Morricone",
-    // srs: "./assets/sounds/Ennio Morricone.mp3"
-    // },
-    // {
-    // title: "River Flows In You",
-    // srs: "./assets/sounds/River Flows In You.mp3"
-    // },
-    // {
-    // title: "Summer Wind",
-    // srs: "./assets/sounds/Summer Wind.mp3"
-    // }
-//]
-// export default playList
-
-// const li = document.createElement('li')
-// li.classList.add('play-item');
-// li.textContent = 'Aqua Caelestis';
-// playListContainer.append('li')
+quote.textContent = dataQuotes[Math.floor(Math.random() * dataQuotes.length)]
+// let quotesUrl = dataQuotes
 
 
-
-
-
-
-// pause.addEventListener('click', pauseAudio )
-
-
-
-    // {
-    // title: "Aqua Caelestis",
-    // srs:  "./assets/sounds/Aqua Caelestis.mp3"
-    // },
-    // {
-    // title: "Ennio Morricone",
-    // srs: "./assets/sounds/Ennio Morricone.mp3"
-    // },
-    // {
-    // title: "River Flows In You",
-    // srs: "./assets/sounds/River Flows In You.mp3"
-    // },
-    // {
-    // title: "Summer Wind",
-    // srs: "./assets/sounds/Summer Wind.mp3"
-    // }
-//]
-// export default playList
-
-// const li = document.createElement('li')
-// li.classList.add('play-item');
-// li.textContent = 'Aqua Caelestis';
-// playListContainer.append('li')
-
-
-
-
-
-
-// const audio = new Audio()
-// playNum = 0
-// let isPlay = false
-
-// function playAudio() {
-//     if (!isPlay) {
-//         audio.src = "./assets/sounds/Aqua Caelestis.mp3"
-     
-//         audio.current = 0
-       
-       
-//         audio.play()
-       
-     
-//    isPlay = true
-   
-        
-//     }
-//    else if (isPlay)  {
-//         audio.pause()
-//       
+function quoteFn() {
+  quote.textContent = dataQuotes[Math.floor(Math.random() * dataQuotes.length)]
   
-//  isPlay = false
-//     }
-// }
+}
 
-
-// function toggleBtn(){
-//     play.classList.toggle('pause')
-// }
-
-// play.addEventListener('click', toggleBtn)
-
-
-
-// const playList = ['srs: "./assets/sounds/Aqua Caelestis.mp3"', 'srs: "./assets/sounds/Ennio Morricone.mp3"', 'srs: "./assets/sounds/River Flows In You.mp3"', 'srs:"./assets/sounds/Summer Wind.mp3"']
-// function listPlay(playList) {
-//     audio.src = `"./assets/sounds/${playList}.mp3"`
-// }
-
-// function lastAudio() {
-   
- 
-   
-//     play.classList.add('pause')
-//     if (playNum === 0) {
-        
-//         if (!isPlay || isPlay ) {
-//             audio.src = "./assets/sounds/Ennio Morricone.mp3"
-//           
-//             audio.play()
-//             playNum++
-           
-//             console.log('111')
-            
-//         }
-         
-//     }
-          
-       
-//    else if (playNum === 1 )  {
-    
-//         audio.src = "./assets/sounds/River Flows In You.mp3"
-//         
-         
-//           audio.play()
-//         console.log('222')
-//         playNum++
-       
-//     }
-    
-//     else if (playNum === 2 ) {
-    
-//         audio.src = "./assets/sounds/Summer Wind.mp3"
-//          
-//           audio.play()
-//         console.log('333')
-//         playNum++
-        
-//     }
-    
-//     else if (playNum === 3 ) {
-    
-//         audio.src = "./assets/sounds/Aqua Caelestis.mp3"
-//            
-//           audio.play()
-//         console.log('444')
-//         playNum = 0
-       
-//     }
-      
-// }
-
-// let lastNum = 4
-// function firstAudio() {
-
-//     play.classList.add('pause')
-//     if (lastNum === 4) {
-        
-//         if (!isPlay || isPlay) {
-//             audio.src = "./assets/sounds/Summer Wind.mp3"
-//            
-        
-//             audio.play()
-//             lastNum--
-//             console.log('444')
-//             isPlay = true
-//         }
-         
-//     }
-          
-       
-//    else if (lastNum === 3 )  {
-    
-//         audio.src = "./assets/sounds/River Flows In You.mp3"
-//       
-//           audio.play()
-//         console.log('33')
-//         lastNum--
-      
-//     }
-    
-//     else if (lastNum === 2 ) { 
-    
-//         audio.src = "./assets/sounds/Ennio Morricone.mp3"
-        
-//          
-//           audio.play()
-//         console.log('22')
-//         lastNum--
-       
-//     }
-    
-//     else if (lastNum === 1 ) {
-    
-//         audio.src = "./assets/sounds/Aqua Caelestis.mp3"
-        
-//           
-//           audio.play()
-//         console.log('11')
-//         lastNum = 4
-//     }
-// }
-// play.addEventListener('click', playAudio)
-// last.addEventListener('click', lastAudio)
-// first.addEventListener('click', firstAudio)
- 
-//  addEventListener('ended', (event) => {
-//     audio.src = "./assets/sounds/Ennio Morricone.mp3" 
-//  })
+ changeQuote.addEventListener('click', quoteFn)
